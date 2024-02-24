e66faf46f45e6dba9dc00d2d094ba1f6db941337
Kartavya Vashishtha <sendtokartavya@gmail.com>
2024-02-24 12:06:33 -0500

    it works

--- src/jit.rs
+++ src/jit.rs
@@ -7,9 +7,9 @@ use crate::{value::Value, vm::{ByteCodeChunk, Op, VMError, VM}};
 
 pub struct JIT {
 	chunk: ByteCodeChunk,
-	stack: Vec<Value>
 }
 
+#[derive(Debug)]
 pub enum CompileError {
 	OutOfBoundsConst(usize)
 }
@@ -32,75 +32,63 @@ macro_rules! mdynasm {
     }
 }
 
+/// Loads arg1 and arg2 into x0 and x1, and stores the result in x0.
+macro_rules! two_arg_one_ret {
+    ($ops:ident $($t:tt)*) => {
+        mdynasm!($ops
+			; ldr x0, [sp], #16
+			; ldr x1, [sp], #16
+            $($t)*
+			; str x0, [sp, #-16]!
+        )
+    }
+}
+
 impl JIT {
 	pub fn new(chunk: ByteCodeChunk) -> Self {
 		JIT {
 			chunk,
-			stack: Vec::new()
 		}
 	}
 
-	pub fn compile(self) -> Result<(), CompileError> {
+	pub fn compile_and_run(self) -> Result<i64, CompileError> {
 		debug!("Starting JIT");
 		let mut ops = aarch64::Assembler::new().unwrap();
 		let consts = self.chunk.consts;
 		
-		let string = "Hello, world!\n";
-
-		// for op in self.chunk.code {
-		// 	match op {
-		// 		Op::Constant { idx } => {
-		// 			let constant = consts.get(idx).ok_or(CompileError::OutOfBoundsConst(idx))?;
-		// 			let num = constant.0;
-		// 			mdynasm!(ops
-		// 				; mov x0, num as u64
-		// 				; str x0, [sp, #-16]!
-		// 			);
-		// 		},
-		// 		Op::Return => {
-		// 			mdynasm!(ops
-		// 				; ldr x0, [sp], #16
-		// 				; ret
-		// 			);
-		// 		},
-		// 		Op::Add => {
-		// 			mdynasm!(ops
-		// 				; ldr x0, [sp], #16
-		// 				; ldr x1, [sp], #16
-		// 				; add x0, x0, x1
-		// 				; str x0, [sp, #-16]!
-		// 			);
-		// 		}
-		// 	}
-		// }
-
-		dynasm!(
-			ops
-			; .arch aarch64
-			; ->hello:
-			; .bytes string.as_bytes()
-			; .align 4 // LDR requires 4-byte alignment
-			; ->print:
-			; .qword print as _
-			; .align 4
-		);
-
-		let hello = ops.offset();
-		dynasm!(ops
-			; .arch aarch64
-			; adr x0, -> hello            // Message.
-			; mov x1, string.len() as u64 // Length.
-			; ldr x2, -> print            // Address of the `print` function.
-			; br x2                       // Call the function (actually tail call).
-		);
+		let offset = ops.offset();
+		for op in self.chunk.code {
+			match op {
+				Op::Constant { idx } => {
+					let constant = consts.get(idx).ok_or(CompileError::OutOfBoundsConst(idx))?;
+					let num = constant.0;
+					mdynasm!(ops
+						; mov x0, num as u64
+						; str x0, [sp, #-16]!
+					);
+				}
+				Op::Add => {
+					mdynasm!(ops
+						; ldr x0, [sp], #16
+						; ldr x1, [sp], #16
+						; add x0, x0, x1
+						; str x0, [sp, #-16]!
+					);
+				}
+				Op::Return => {
+					mdynasm!(ops
+						; ldr x0, [sp], #16
+						; ret
+					);
+				}
+			}
+		}
 
 		let buf = ops.finalize().unwrap();
-		let hello_fn: extern "C" fn() -> () = unsafe { std::mem::transmute(buf.ptr(hello)) };
+		let compiled: unsafe extern "C" fn() -> i64 = unsafe { std::mem::transmute(buf.ptr(offset)) };
 
-		hello_fn();
-
-		println!("it worked!");
-		Ok(())
+		// Safety: the ExecutableBuffer is alive and valid.
+		Ok(unsafe { compiled() })
 	}
 }
 
@@ -111,10 +99,19 @@ mod jit_tests {
 	#[test]
 	fn test_jit() {
 		let chunk = ByteCodeChunk {
-			consts: vec![],
-			code: vec![]
+			consts: vec![
+				Value(2),
+				Value(2)
+			],
+			code: vec![
+				Op::Constant { idx: 0 },
+				Op::Constant { idx: 1 },
+				Op::Add,
+				Op::Return
+			]
 		};
 		let jit = JIT::new(chunk);
-		assert!(jit.compile().is_ok());
+		let ret = jit.compile_and_run().unwrap();
+		assert_eq!(ret, 4);
 	}
 }
